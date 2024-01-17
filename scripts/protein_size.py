701b352d5a708ebdf3f672c0a59ad58fa3c90c69
Aron Broom <broomsday@gmail.com>
2024-01-16 23:35:03 -0500

    Compute radii of gyration for the standards and samples

--- scripts/protein_size.py
+++ scripts/protein_size.py
@@ -10,23 +10,73 @@ from pathlib import Path
 import typer
 import pandas as pd
 
+from sizer.sizer import compute_radius_of_gyration
+
+
+def get_structure_path(pdb_name: str, pdb_top_dir: Path) -> Path:
+    """
+    Given the name of a PDB file and the PDB dir, guess the PDB Path.
+    """
+    top_dir = Path(pdb_top_dir)
+    for file_path in top_dir.rglob(pdb_name):
+        return file_path
+
+    return None
+
 
 def main(
-    standards: Path = typer.Argument(..., help=".csv of stanard elution times and structures"),
+    standards: Path = typer.Argument(
+        ..., help=".csv of stanard elution times and structures"
+    ),
     sample_elutions: Path = typer.Argument(..., help=".csv of sample elution times"),
-    sample_structures: Path = typer.Argument(..., help=".csv of sample structures and oligomer states"),
+    sample_structures: Path = typer.Argument(
+        ..., help=".csv of sample structures and oligomer states"
+    ),
+    pdb_dir: Path = typer.Argument(..., help="Where all the PDB structures are held"),
+    out_dir: Path = typer.Argument(..., help="Where to save output .csvs"),
 ):
     """
     Compute the expected oligomerization of a protein based on SEC elution time(s).
     """
-    standards_data = pd.read_csv(standards)
+    # load data
+    standard_data = pd.read_csv(standards)
     sample_elution_data = pd.read_csv(sample_elutions)
     sample_structure_data = pd.read_csv(sample_structures)
 
-    print(standards_data)
+    # compute radius of gyration
+    standard_data["structure_path"] = standard_data["structure"].apply(
+        get_structure_path, args=[pdb_dir]
+    )
+    standard_data["radius_gyration"] = standard_data["structure_path"].apply(
+        compute_radius_of_gyration
+    )
+    standard_data = standard_data.drop(labels=["structure_path"], axis=1)
+
+    sample_structure_data["structure_path"] = sample_structure_data["structure"].apply(
+        get_structure_path, args=[pdb_dir]
+    )
+    sample_structure_data["radius_gyration"] = sample_structure_data[
+        "structure_path"
+    ].apply(compute_radius_of_gyration)
+    sample_structure_data = sample_structure_data.drop(
+        labels=["structure_path"], axis=1
+    )
+
+    # TODO: analyze
+    # slope, intercept = np.polyfit(x, y, 1)
+    # correlation_matrix = np.corrcoef(x, y)
+    # correlation_xy = correlation_matrix[0,1]
+    # r_squared = correlation_xy**2
+
+    # TODO: report
+    out_dir.mkdir(exist_ok=True)
+    standard_data.to_csv(out_dir / "standards.csv")
+    sample_structure_data.to_csv(out_dir / "structures.csv")
+    sample_elution_data.to_csv(out_dir / "elutions.csv")
+
+    print(standard_data)
     print(sample_elution_data)
     print(sample_structure_data)
-
 
 
 if __name__ == "__main__":
--- sizer/sizer.py
+++ sizer/sizer.py
@@ -1,3 +1,18 @@
 """
 Functions towards computing accurate oligomer states from SEC data of non-globular designs.
 """
+
+
+from pathlib import Path
+
+import biotite.structure as bts
+from biotite.structure.io import load_structure
+
+
+def compute_radius_of_gyration(structure_file: Path) -> float:
+    """
+    Return the radius of gyration for the structure in a PDB.
+    """
+    structure = load_structure(structure_file)
+
+    return bts.gyration_radius(structure)
