e19d16a9bb3dd79583282e876c70af1ed8e374a7
Aron Broom <broomsday@gmail.com>
2024-01-20 23:09:28 -0500

    Compute partition coefficient and estimate samples

--- scripts/protein_size.py
+++ scripts/protein_size.py
@@ -9,8 +9,15 @@ from pathlib import Path
 
 import typer
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-from sizer.sizer import compute_radius_of_gyration
+from sizer.sizer import (
+    compute_radius_of_gyration,
+    compute_partition_coefficient,
+    estimate_radius_of_gyration,
+    get_closest_oligomer,
+)
 
 
 def get_structure_path(pdb_name: str, pdb_top_dir: Path) -> Path:
@@ -32,16 +39,24 @@ def main(
     sample_structures: Path = typer.Argument(
         ..., help=".csv of sample structures and oligomer states"
     ),
+    column_parameters: Path = typer.Argument(
+        ..., help=".csv giving the total and void volumes of the column"
+    ),
     pdb_dir: Path = typer.Argument(..., help="Where all the PDB structures are held"),
     out_dir: Path = typer.Argument(..., help="Where to save output .csvs"),
+    debug: bool = typer.Option(default=False, help="Show standard curve plot"),
 ):
     """
     Compute the expected oligomerization of a protein based on SEC elution time(s).
+
+    Example:
+    python scripts/protein_size.py data/nanopore_2XQS/standards.csv data/nanopore_2XQS/elutions.csv data/nanopore_2XQS/structures.csv data/nanopore_2XQS/pdbs/ data/nanopore_2XQS/output
     """
     # load data
     standard_data = pd.read_csv(standards)
     sample_elution_data = pd.read_csv(sample_elutions)
     sample_structure_data = pd.read_csv(sample_structures)
+    column_parameter_data = pd.read_csv(column_parameters)
 
     # compute radius of gyration
     standard_data["structure_path"] = standard_data["structure"].apply(
@@ -62,21 +77,65 @@ def main(
         labels=["structure_path"], axis=1
     )
 
-    # TODO: analyze
-    # slope, intercept = np.polyfit(x, y, 1)
-    # correlation_matrix = np.corrcoef(x, y)
-    # correlation_xy = correlation_matrix[0,1]
-    # r_squared = correlation_xy**2
+    # pull out needed column parameters
+    void_volume = (
+        column_parameter_data[column_parameter_data["parameter"] == "void"]
+        .iloc[0]
+        .elution
+    )
+    total_volume = (
+        column_parameter_data[column_parameter_data["parameter"] == "total"]
+        .iloc[0]
+        .elution
+    )
 
-    # TODO: report
+    # compute the partition coefficient for each elution volume
+    standard_data["partition"] = standard_data["elution"].apply(
+        compute_partition_coefficient, args=(void_volume, total_volume)
+    )
+    sample_elution_data["partition"] = sample_elution_data["elution"].apply(
+        compute_partition_coefficient, args=(void_volume, total_volume)
+    )
+
+    # save the standards and structures rg calculations
     out_dir.mkdir(exist_ok=True)
     standard_data.to_csv(out_dir / "standards.csv")
     sample_structure_data.to_csv(out_dir / "structures.csv")
+
+    # a plot of K vs. Log10(size) (e.g radius of gyration) is expected to be linear
+    if debug:
+        plt.scatter(
+            standard_data["partition"], np.log10(standard_data["radius_gyration"])
+        )
+        plt.show()
+    slope, intercept = np.polyfit(
+        standard_data["partition"], np.log10(standard_data["radius_gyration"]), 1
+    )
+    correlation_matrix = np.corrcoef(
+        standard_data["partition"], np.log10(standard_data["radius_gyration"])
+    )
+    correlation_xy = correlation_matrix[0, 1]
+    r_squared = correlation_xy**2
+
+    print(f"Standard curve R^2: {r_squared}\n")
+
+    # use the slope and intercept to compute the radius_gyration for the samples
+    sample_elution_data["radius_gyration"] = sample_elution_data["partition"].apply(
+        estimate_radius_of_gyration, args=(slope, intercept)
+    )
+
+    # use the radius_gyration of the structures to estimate the closest matching oligomer
+    sample_elution_data["closest_oligomer"] = sample_elution_data[
+        "radius_gyration"
+    ].apply(get_closest_oligomer, args=(sample_structure_data,))
+
+    # save the estimated sample data
     sample_elution_data.to_csv(out_dir / "elutions.csv")
 
-    print(standard_data)
-    print(sample_elution_data)
-    print(sample_structure_data)
+    # report the data
+    print(standard_data, "\n")
+    print(sample_elution_data, "\n")
+    print(sample_structure_data, "\n")
 
 
 if __name__ == "__main__":
--- sizer/sizer.py
+++ sizer/sizer.py
@@ -5,6 +5,8 @@ Functions towards computing accurate oligomer states from SEC data of non-globul
 
 from pathlib import Path
 
+import pandas as pd
+
 import biotite.structure as bts
 from biotite.structure.io import load_structure
 
@@ -16,3 +18,37 @@ def compute_radius_of_gyration(structure_file: Path) -> float:
     structure = load_structure(structure_file)
 
     return bts.gyration_radius(structure)
+
+
+def compute_partition_coefficient(elution: float, void: float, total: float) -> float:
+    """
+    Compute the SEC partition coefficient.
+
+    K = (Ve - Vo) / (Vt - Vo)
+    Ve = elution volume (peak max height)
+    Vt = total volume, computed as pi*h*r^2 (cylindrical volume) of the packed bed
+    Vo = void volume, elution volume of something so large as to be completely excluded
+
+    https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8410290/
+    """
+    return (elution - void) / (total - void)
+
+
+def estimate_radius_of_gyration(
+    partition: float, slope: float, intercept: float
+) -> float:
+    """
+    Given linear fit parameters and the partition coefficient, estimate the radius of gyration.
+    """
+    log_10_rg = (slope * partition) + intercept
+    return 10**log_10_rg
+
+
+def get_closest_oligomer(radius_gyration: float, structure_data: pd.DataFrame) -> int:
+    """
+    Report the oligomer structure that most closely matches the radius of gyration.
+    """
+    structure_data["diff"] = abs(structure_data["radius_gyration"] - radius_gyration)
+    structure_data = structure_data.sort_values(by="diff")
+
+    return structure_data.iloc[0].oligomer
